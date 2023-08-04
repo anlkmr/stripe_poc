bd11e45488dbe748d4e89e1350439f6bdf819b3c
anlkmr <anil.kadiga@emagia.com>
2023-08-04 21:05:23 +0530

    initial

---
+++ src/main/java/com/poc/stripe_poc/StripePocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.stripe_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripePocApplication.class, args);
+	}
+
+}
