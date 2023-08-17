fa2d1593ca4d96415846a8c8a7974cc12db4502c
anlkmr <anil.kadiga@emagia.com>
2023-08-17 16:44:50 +0530

    new changes

--- src/main/java/com/poc/stripe_poc/controller/StripePaymentController.java
+++ src/main/java/com/poc/stripe_poc/controller/StripePaymentController.java
@@ -22,6 +22,34 @@ public class StripePaymentController {
     @Value("${stripe.apikey}")
     String stripeKey;
 
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/customer")
+    public String customer(Model model) throws StripeException {
+        Stripe.apiKey = stripeKey;
+        //CustomerListParams params = CustomerListParams.builder().build();
+        //Map<String, Object> params = new HashMap<>();
+        CustomerListParams params = CustomerListParams.builder()
+                //.setEmail("use the stored email")
+                .setLimit(3L)
+                .build();
+        //params.put("limit", 3);
+        CustomerCollection customers = Customer.list(params);
+        List<CustomerData> allCustomers = new ArrayList<CustomerData>();
+        for(Customer customerResult:customers.getData()){
+            CustomerData customerData = new CustomerData();
+            customerData.setCustomerId(customerResult.getId());
+            customerData.setName(customerResult.getName());
+            customerData.setDescription(customerResult.getDescription());
+            customerData.setEmail(customerResult.getEmail());
+            allCustomers.add(customerData);
+        }
+        model.addAttribute("customers", allCustomers);
+        return "customer";
+    }
     @RequestMapping("/index")
     public String index(Model model) throws StripeException {
         Stripe.apiKey = stripeKey;
